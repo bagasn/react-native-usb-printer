e9d17b5d022052dfcc86ffd67eecc5b0948a37a5
bagasn <bagasnasution@gmail.com>
2023-05-29 13:07:50 +0700

    Fixing: set broadcast receiver set as mutable flag

--- android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/USBPrinterAdapter.java
@@ -89,7 +89,12 @@ public class USBPrinterAdapter {
     public void init(Context reactContext) {
         this.mContext = reactContext;
         this.mUSBManager = (UsbManager) this.mContext.getSystemService(Context.USB_SERVICE);
-        this.mPermissionIndent = PendingIntent.getBroadcast(mContext, 0, new Intent(ACTION_USB_PERMISSION), (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? PendingIntent.FLAG_IMMUTABLE : 0);
+        this.mPermissionIndent = PendingIntent.getBroadcast(
+            mContext, 
+            0, 
+            new Intent(ACTION_USB_PERMISSION), 
+            (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) ? PendingIntent.FLAG_MUTABLE : 0
+        );
         IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
         mContext.registerReceiver(mUsbDeviceReceiver, filter);
